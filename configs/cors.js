cec663bd8033fcb02879077bace9c7ec72165d08
Sampreet-Chawla <sam.learnnew@gmail.com>
2020-11-22 20:25:46 -0500

    Relaxed CORS setting for Postman testing.

--- configs/cors.js
+++ configs/cors.js
@@ -1,6 +1,10 @@
 // The White List Array are urls allowed to make API requests in production mode
 // IN dev mode "npm run dev" any source can make a request
-var whitelist = ["https://smoothies2go.netlify.app/", "http://localhost:3000/"];
+var whitelist = [
+  "https://smoothies2go.netlify.app/",
+  "http://localhost:3000/",
+  "*",
+];
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
