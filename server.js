7d3262bece9bc3c99555b16e922e3f10417fbc7c
Sampreet-Chawla <sam.learnnew@gmail.com>
2020-11-22 20:47:49 -0500

    Relaxed CORS setting for Postman testing - for all.

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const corsOptions = require("./configs/cors.js");
 const mongoose = require("./db/conn");
 
 // Add the middleware code needed to accept incoming data and add it to req.body
-NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors());
+//NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors());
+// Enabled to allow POSTMAN testing. 
+app.use(cors();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("tiny"));
